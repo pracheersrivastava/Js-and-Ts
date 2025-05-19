0b6de3bac45721f5d1354e5f72905472d53949db
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-19 17:33:47 +0530

    Add files via upload

---
+++ src/type-alias.ts
@@ -0,0 +1,15 @@
+// Improving objects.ts code
+type EmployeeType = {
+    readonly id: number;
+    name : string;
+    retire : (date: Date) => void
+}
+
+
+let employee2 : EmployeeType ={
+    id : 1,
+    name : "Pracheer",
+    retire : (date : Date) => {
+        console.log(date);
+    }
+}
