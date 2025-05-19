21d1206b4a37c832e185d2e7dcb1dc81e7ef1bcb
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-19 17:46:09 +0530

    Add files via upload

---
+++ src/intersection-types.ts
@@ -0,0 +1,13 @@
+type Draggable = {
+    drag: () => void
+};
+
+type Resizeable = {
+    resize : () => void
+}
+
+type UIWidget = Draggable & Resizeable;
+let textBox : UIWidget = {
+    drag : () => {},
+    resize : () => {}
+}
