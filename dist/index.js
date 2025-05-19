6f4d46b84f10600718fd1e3f2315ea3fd2d418df
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-19 16:25:58 +0530

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+//Basic Types
 let id = 5;
 let company = "Dynamatrix";
 let isPublished = true;
@@ -8,15 +9,19 @@ age = 40;
 let ids = [1, 2, 3, 4, 5, 6];
 let arr = [1, 'hello', true];
 arr.push('hi');
+//tupple
 let person = [1, 'Jon', true];
+//Tupple Array
 let employee;
 employee = [
     [1, 'brad'],
     [2, 'Jon'],
     [3, 'Mon']
 ];
+//Union
 let pid;
 pid = '22';
+//Enum
 var Direction1;
 (function (Direction1) {
     Direction1[Direction1["Up"] = 1] = "Up";
@@ -39,8 +44,11 @@ const user = {
     id: 1,
     name: 'John'
 };
+//Type Assertion
 let cid = 1;
+// let customerId = <number>cid
 let customerId = cid;
+//Functions
 function addNum(x, y) {
     return x + y;
 }
@@ -55,6 +63,7 @@ const user1 = {
 };
 const add = (x, y) => x + y;
 const sub = (x, y) => x - y;
+//Classes
 class Person {
     id;
     name;
@@ -71,6 +80,7 @@ const mon = new Person(2, "Mon");
 console.log(jon.register());
 console.log(jon, mon);
 jon.id = 5;
+//Subclasss
 class Employee extends Person {
     position;
     constructor(id, name, position) {
@@ -80,10 +90,10 @@ class Employee extends Person {
 }
 const emp = new Employee(3, 'DD', 'Developer');
 console.log(emp.register());
+//Generics
 function getArray(items) {
     return new Array().concat(items);
 }
 let numArray = getArray([1, 2, 3, 4]);
 let strArray = getArray(['Shawn', 'Brad', 'John']);
 strArray.push('hello');
-//# sourceMappingURL=index.js.map
