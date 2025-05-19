148c49548371488ea2e7f1678375ea319639716b
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-19 16:39:42 +0530

    Add files via upload

---
+++ src/enums.ts
@@ -0,0 +1,15 @@
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// //Pascal Case
+// enum Size { Small = 1, Medium, Large }
+// let mySize: Size = Size.Medium
+// console.log(mySize)
+
+
+
+//Pascal Case
+const enum Size { Small = 1, Medium, Large }
+let mySize: Size = Size.Medium
+console.log(mySize)
