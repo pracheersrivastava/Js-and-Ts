80ef264f654e0ff2f3d4f261bb8580ddbe9f9851
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-19 17:34:29 +0530

    Add files via upload

---
+++ dist/objects.js
@@ -0,0 +1,4 @@
+"use strict";
+let employee1 = { id: 1, name: 'John', retire: (date) => {
+        console.log(date);
+    } };
---
+++ dist/type-alias.js
@@ -0,0 +1,8 @@
+"use strict";
+let employee2 = {
+    id: 1,
+    name: "Pracheer",
+    retire: (date) => {
+        console.log(date);
+    }
+};
